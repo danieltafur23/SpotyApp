0339710e6b2dcc2262400b253433f1d8b65670b1
yirleyson <sdiazosp@cesde.net>
2021-09-04 13:37:56 -0500

    Priemer Ingreso
    1°

--- servicio.js
+++ servicio.js
@@ -1,6 +1,6 @@
 let uri = "https://api.spotify.com/v1/artists/3JSSjGYcIkgsrz7892CelT/top-tracks?market=US";
 
-let token = "Bearer BQAk3Yu8cceS0s62ZDKEADxkKuz0ZBoAVFyFGNC3sN7sfF43b1ZaV7osTAC8zqmA03xSYUgk8bfyV9ukWsf3cG2_kkKv9WTmnmCv_XawPbaTA3kEW22fAO0ObwwtHYi4aq-PBhF0jp_QcbR5fn-hhCxxQ0gwvpFzfgo";
+let token = "Bearer BQApICPKCLmwH6uE9WGUy5rjRXNwMPBeRxNt2zRcI_Dz6G6c1C_lCUKCVh4XIIwHUqqyogmQGaChT_Fb5_fINaNikKrPR3zHxpelh3XlvmmO27uhtnXIdNq60rnB_kP3Bv7V";
 
 let parametrosPeticion = {
     method:"GET",
@@ -10,13 +10,70 @@ let parametrosPeticion = {
 
 }
 
-fetch(uri,parametrosPeticion )
+fetch(uri,parametrosPeticion ) //Fetch es una promesa
 .then(function(respuesta){
     return (respuesta.json());
 })
 .then(function(respuesta){
-    console.log(respuesta);
+    console.log(respuesta); //Objeto
+
+    pintarDatos(respuesta.tracks);
+
+    //console.log(respuesta.tracks); //Arreglo
+    // console.log(respuesta.tracks[0]); // 
+    //console.log(respuesta.tracks[0].name);
+    //console.log(respuesta.tracks[0].preview_url); 
+    //console.log(respuesta.tracks[0].album.images[0].url);
 })
 .catch(function(error){
     console.log(error);
 })
+
+function pintarDatos(datos){
+
+    let fila=document.getElementById("fila");
+
+    datos.forEach(function(cancion){
+        
+        let columna = document.createElement("div");
+        columna.classList.add("col");
+
+        let tarjeta = document.createElement("div");
+        columna.classList.add("card");
+        columna.classList.add("h-100");
+
+        let imagen = document.createElement("img");
+        imagen.classList.add("card-img-top");
+        imagen.src=cancion.album.images[0].url;
+
+        let cardBody = document.createElement("div");
+        cardBody.classList.add("card-body");
+
+        let cardTitle = document.createElement("h5");
+        cardTitle.classList.add("card-title");
+        cardTitle.textContent=cancion.name;
+
+        let cardPopularidad = document.createElement("h6");
+        cardPopularidad.classList.add("card-title");
+        cardPopularidad.textContent="Popularidad: " + cancion.popularity;
+
+        let cardFooter = document.createElement("div");
+        cardFooter.classList.add("card-footer");
+
+        let cardAudio = document.createElement("audio");
+        cardAudio.classList.add("barra-audio");
+        cardAudio.setAttribute("controls", "controls");
+        cardAudio.src=cancion.preview_url;
+
+        //PADRES E HIJOS
+
+        tarjeta.appendChild(imagen);
+        tarjeta.appendChild(cardBody);
+        tarjeta.appendChild(cardFooter);
+        cardBody.appendChild(cardTitle);
+        cardBody.appendChild(cardPopularidad);
+        cardFooter.appendChild(cardAudio);
+        columna.appendChild(tarjeta);
+        fila.appendChild(columna);
+    })
+}
